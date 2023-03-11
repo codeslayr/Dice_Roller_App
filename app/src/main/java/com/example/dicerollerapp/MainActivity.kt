8a2c1fcfbb859fd21bcf570063cafef87a0cf063
Weirdo <ansariabdurrahman@gmail.com>
2023-03-11 15:45:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.dicerollerapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.dicerollerapp.ui.theme.DiceRollerAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerAppTheme {
+                DiceWithButtonAndImage()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+
+}
+@Composable
+fun DiceWithButtonAndImage() {
+    var result by remember { mutableStateOf(1) }
+
+    val image = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(painter = painterResource(id = image) , contentDescription = "Dice Image" )
+
+        Button(
+            onClick = { result = (1..6).random() },
+        ) {
+            Text(
+                text = "Roll"
+            )
+            
+        }
+
+    }
+
+}
